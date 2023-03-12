06b6fd09fc0cde073ba627313f9807f1ae44669d
Ayush Kumar <ayushkumar4975@gmail.com>
2023-03-12 22:30:56 +0530

    commiting for the first time

---
+++ api-search.php
@@ -0,0 +1,35 @@
+<?php
+
+
+header('Content-Type: application/json');
+header('Acess-Control-Allow-Origin: *');
+
+//for POST
+// $data = json_decode(file_get_contents("php://input"), true);
+
+// $search_value = $data['search'];
+
+
+
+
+//for GET (from URL bar)(not in json)
+$search_value=isset($_GET['search'])?$_GET['search']:die();
+
+
+
+
+include "config.php";
+
+$sql = "SELECT * FROM students WHERE student_name LIKE '%{$search_value}%'";
+
+$result = mysqli_query($conn, $sql) or die("SQL Query Failed");
+
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+} else {
+    echo json_encode(array('message' => 'NO search Found', 'status' => false));
+}
+
+
+?>
